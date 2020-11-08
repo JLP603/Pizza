c95ccf248b3aa2dd087f303ab18836de905ae7b2
JLP603 <60498189+JLP603@users.noreply.github.com>
2020-11-08 22:54:42 +0800

    made some changes to project base

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
+// All imports needed here
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const handlebars = require('handlebars');
+
+// Creates the express application
 const app = express();
 const port = 3000;
-//Loads the handlebars module
-const exphandle = require('express-handlebars');
-const handlebars = require('express-handlebars');
 
-app.engine('hbs', exphandle({
-    extname: 'hbs',
-    defaultView: 'main',
+/**
+  Creates an engine called "hbs" using the express-handlebars package.
+**/
+app.engine( 'hbs', exphbs({
+    extname: 'hbs', // configures the extension name to be .hbs instead of .handlebars
+    defaultView: 'main', // this is the default value but you may change it to whatever you'd like
     layoutsDir: path.join(__dirname, '/views/layouts'), // Layouts folder
     partialsDir: path.join(__dirname, '/views/partials'), // Partials folder
-}));
-app.set('views', path.join(__dirname, 'views'));
+  }));
+
+// Setting the view engine to the express-handlebars engine we created
 app.set('view engine', 'hbs');
 
+// Routes
+app.get('/', function(req, res) {
+  res.render('about')
+});
+/**
+  To be able to render images, css and JavaScript files, it's best to host the static files
+  and use the expected path in the data and the imports.
 
+  This takes the contents of the public folder and makes it accessible through the URL.
+  i.e. public/css/styles.css (in project) will be accessible through http://localhost:3000/css/styles.css
+**/
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-res.render('main', {layout : 'index'});
-});
-
-app.listen(port, () => console.log(`App listening to port ${port}`));
+// Listening to the port provided
+app.listen(port, function() {
+    console.log('App listening at port '  + port)
+  });
