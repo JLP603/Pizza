fd1a2ef52f84a3f10f383066a08b402bfe2f9a7f
MiguelLeyesa <61416986+MiguelLeyesa@users.noreply.github.com>
2020-12-07 19:07:21 +0800

    Delete UserOrdersScript.js

--- public/scripts/UserOrdersScript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  // update grand total
-  var grandtotal = parseFloat(0);
-  for (var i = 0; i < $(".detail").length; i++) {
-    grandtotal += parseFloat($(".total").eq(i).html().substring(4, $(".total").eq(i).html().length));
-  }
-  $("#grandtotal").html("Php " + grandtotal.toFixed(2));
-});
